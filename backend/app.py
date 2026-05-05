a5f234e448cf8d286f4042eab22b6576d1de4e4a
Mousoom07 <mousoomsamanta07@gmail.com>
2026-05-05 17:44:05 +0530

    fix render port

--- backend/app.py
+++ backend/app.py
@@ -44,4 +44,5 @@ def index():
 
 if __name__ == '__main__':
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
